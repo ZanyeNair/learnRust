bac2f7204fcde5d546385daee56cd126b0e07c4d
ZanyeNair <zayne.nair@circuitrunners.com>
2023-10-31 16:08:22 -0400

    freaky grils on a yatch

--- learn_cargo/src/main.rs
+++ learn_cargo/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     println!("Please input your guess.");
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("Failed to read number");
-    prinln!("You guessed: {guess}");  
+    println!("You guessed: {guess}");  
 }
 fn new() {
     println!("poop")
